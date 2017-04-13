60fcdd2d1188b3dabab67238dd5afb8bbae0d0c7
izahariev <izahariev96@gmail.com>
2017-04-13 17:59:59 +0300

    peak finder refactored

--- peak_finder/peak_finder_one_dimensional.erl
+++ peak_finder/peak_finder_one_dimensional.erl
@@ -1,10 +1,9 @@
 -module(peak_finder_one_dimensional). 
--export([peak/1,p/1]). 
+-export([p/1]). 
 
 % Peak definition: b is a peak if and only if b >= a and b >= c %
 
 % return the first found peak in a list %
-p([H|T]) -> p(H, T).
-p(A, [B|_]) when A >= B -> A;
-p(A, []) -> A;
-p(_, [A|T]) -> p(A, T).
+p([A|[B|_]]) when A >= B -> A;
+p([A|[]]) -> A;
+p([_|[A|T]]) -> p([A|T]).
